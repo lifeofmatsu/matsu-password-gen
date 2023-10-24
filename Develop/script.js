2345f233c1a914228efc5cc33715b7ad41c66647
Jus Ferrell <j1ferrel@ucsd.edu>
2023-10-24 00:10:10 -0700

    adds global variables, declares functions and helper functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,41 @@
-// Assignment code here
+var pwdLength = 0;
+var characterArray = [];
+
+var lowercaseInclusion = false;
+var uppercaseInclusion = false;
+var numericInclusion = false;
+var symbolicInclusion = false;
+
+var lowercaseGroup = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var uppercaseGroup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var numericGroup = '0123456789'.split('');
+var symbolicGroup = '!()-.?[]_`~;:@#$%^&*+='.split(''); 
 
 
-// Get references to the #generate element
+function generatePassword() {
+    passwordLength();
+    characterSet();
+}
+
+//get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+//add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+function passwordLength() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function characterSet() {
+
+}
